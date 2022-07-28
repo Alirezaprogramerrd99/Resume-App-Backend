23930c69eeddf532a92c534c86e325b674638e15
Alireza Rashidi <arashidi1378@gmail.com>
2022-07-28 13:30:19 +0430

    added JWT demo to project.

---
+++ demos/JWT/db.py
@@ -0,0 +1,10 @@
+fake_users_db = {
+
+    "arashidi1378": {
+        "username": "arashidi1378",
+        "full_name": "Alireza Rashidi",
+        "email": "arashidi1378@gmail.com",
+        "hashed_password": "$2y$10$9qm9gZ92F/uQOxYZBDcqhOPIiNavRqaCRTdFz.Fq1idx1TYEHCA9K",
+        "is_active": True,
+    }
+}
---
+++ demos/JWT/schemas.py
@@ -0,0 +1,22 @@
+from typing import Union
+from pydantic import BaseModel
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    is_active: Union[bool, None] = None
+
+class UserInDB(User):
+    hashed_password: str
